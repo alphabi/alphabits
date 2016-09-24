8ddc1a58ecfc6f6d148101920a9dde1ae03f75ac
Stuart P. Bentley <stuart@testtrack4.com>
2016-09-24 03:05:55 +0000

    listwords: weave pre-sorted dictionaries line by line

--- listwords.lua
+++ listwords.lua
@@ -1,15 +1,30 @@
 local prefix = 'bitdicts/'
 local bits = arg[1]
-local words = {}
-for i = 1, #bits do
-  local dictfile = io.open(prefix .. bits:sub(1, i))
-  if dictfile then
-    for word in dictfile:lines() do
-      words[#words+1] = word
-    end
+local tips = {}
+
+function least(t)
+  local lk, lv = next(t)
+  local ck, cv = next(t, lk)
+  while ck do
+    if cv < lv then lk, lv = ck, cv end
+    ck, cv = next(t, ck)
   end
+  return lk
 end
-table.sort(words)
-for i = 1, #words do
-  print(words[i])
+function readtip(file)
+  local tip = file:read()
+  tips[file] = tip
+  if not tip then file:close() end
+end
+
+for i = 1, #bits do
+  local file = io.open(prefix .. bits:sub(1, i))
+  if file then readtip(file) end
+end
+
+local topfile = least(tips)
+while topfile do
+  print(tips[topfile])
+  readtip(topfile)
+  topfile = least(tips)
 end
