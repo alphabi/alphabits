1558d3a2d396abb2d71e0be683abc954b9f1546f
Stuart P. Bentley <stuart@testtrack4.com>
2018-05-22 22:12:15 +0000

    Teach listwords.lua '-x' option to take input in hex

--- listwords.lua
+++ listwords.lua
@@ -1,13 +1,34 @@
 local prefix = 'bitdicts/'
 local r = false
-if arg[1] == '-r' then
-  r = true
-  table.remove(arg, 1)
+local dehex = false
+while arg[1] and arg[1]:sub(1, 1) == '-' do
+  if arg[1] == '-r' then
+    r = true
+    table.remove(arg, 1)
+  elseif arg[1] == '-x' then
+    dehex = true
+    table.remove(arg, 1)
+  else
+    io.stderr:write(string.format('Unrecognized option %q\n', arg[1]))
+    os.exit(1)
+  end
+end
+
+local hexbits = {}
+for i = 0, 16 do
+  hexbits[string.format('%x',i)] =
+    (i     > 7 and '1' or '0') ..
+    (i % 8 > 3 and '1' or '0') ..
+    (i % 4 > 1 and '1' or '0') ..
+    (i % 2 > 0 and '1' or '0')
 end
 local bits = arg[1]
+if dehex then
+  bits = bits:lower():gsub('.', hexbits)
+end
 local tips = {}
 
-function least(t)
+local function least(t)
   local lk, lv = next(t)
   local ck, cv = next(t, lk)
   while ck do
@@ -16,7 +37,7 @@ function least(t)
   end
   return lk
 end
-function readtip(file)
+local function readtip(file)
   local tip = file:read()
   tips[file] = tip
   if not tip then file:close() end
