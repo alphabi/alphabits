abdada61305e02fb363a34f1f3ba688be3821875
Stuart P. Bentley <s@stuartpb.com>
2020-03-14 19:40:20 -0700

    Strip non-value characters in listwords.lua

--- listwords.lua
+++ listwords.lua
@@ -1,12 +1,15 @@
 local prefix = 'bitdicts/'
 local r = false
-local dehex = false
+local bitwidth = 1
 while arg[1] and arg[1]:sub(1, 1) == '-' do
   if arg[1] == '-r' then
     r = true
     table.remove(arg, 1)
+  -- TODO: allow -x5 and -x6 for base 32 and base64
+  -- perhaps also -x3 for octal, and x2 for completion?
+  -- x1 discards all nonbinary characters
   elseif arg[1] == '-x' then
-    dehex = true
+    bitwidth = 4
     table.remove(arg, 1)
   else
     io.stderr:write(string.format('Unrecognized option %q\n', arg[1]))
@@ -22,10 +25,21 @@ for i = 0, 16 do
     (i % 4 > 1 and '1' or '0') ..
     (i % 2 > 0 and '1' or '0')
 end
-local bits = arg[1]
-if dehex then
+
+local bits = arg[1]:gsub('%X','')
+
+-- TODO: skip this under -x6 (the only base with X as a digit)
+if bits:match('^0[xX]') then
+  bits = bits:sub(3)
+  bitwidth = 4
+end
+
+if bitwidth == 4 then
   bits = bits:lower():gsub('.', hexbits)
 end
+
+bits = bits:gsub('[^01]','')
+
 local tips = {}
 
 local function least(t)
